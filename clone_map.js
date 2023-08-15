02167c6831d9c46faa66cee0078577ed1e7e52a4
Amaldev <88559916+amaldev07@users.noreply.github.com>
2023-08-15 11:40:36 +0530

    Create clone_map.js

---
+++ clone_map.js
@@ -0,0 +1,4 @@
+To deep clone(create a duplicate) of a Map in javascript
+
+var newMap = new Map(existingMap)
+/* With the introduction of Maps in JavaScript it's quite simple considering the constructor accepts an iterable: */
