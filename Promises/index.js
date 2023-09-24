6c3dd59847d2578d7ed51534140f4f51df7c027a
Amaldev <amaldev.psn@gmail.com>
2023-09-24 18:17:54 +0530

    c

--- Promises/index.js
+++ Promises/index.js
@@ -1,10 +1,17 @@
-let p = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve(123);
-    }, 2000)
-})
+function p1() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(123);
+        }, 2000)
+    })
+}
+// p1().then(()=>{
+//     console.log("promise resolved")
+// });
 
-async function myAsyncFn(){
+async function myAsyncFn() {
     await p();
     console.log("p resolved")
 }
+
+myAsyncFn();
