87b9255a1880c01569a93219c979d3ad421bc956
Amaldev <amaldev.psn@gmail.com>
2024-09-19 21:42:01 +0530

    c

--- namste_javascript/map.js
+++ namste_javascript/map.js
@@ -1,9 +1,23 @@
-let arr = [2,3,4,5]
+let arr = [2, 3, 4, 5];
 /* we need output as [4,6,8,10]
 here we use Map */
-let output = arr.map((x)=>{
-return x*2;
+let output = arr.map((x) => {
+    return x * 2;
 })
 debugger;
 console.log(output);
+console.log(arr);
+
+arr.map((x) => {
+    return x * 2;
+})
+
+/* same as below */
+arr.map((x) => x * 2);
+
+/* same as below */
+arr.map(myMap);
+function myMap(x) {
+    return x * 2;
+}
 
