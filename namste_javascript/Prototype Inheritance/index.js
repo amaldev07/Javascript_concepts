3aa1256086a8218d2907706c34cc1a78831dbff2
Amaldev <amaldev.psn@gmail.com>
2024-10-03 10:19:11 +0530

    Object Literal vs constructor function

--- namste_javascript/Prototype Inheritance/index.js
+++ namste_javascript/Prototype Inheritance/index.js
@@ -17,3 +17,15 @@ function Person(name, age) {
         console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
     };
 }
+// -------------------------------------
+// diff b/w below 2
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+let person =  new Person("amal", "22");
+// and 
+let person = {
+    name : "amal",
+    age : "22"
+}
