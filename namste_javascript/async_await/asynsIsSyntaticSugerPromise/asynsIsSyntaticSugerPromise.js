18f64f3e5ef3726efeb22dc8b3a5a064153e6a69
Amaldev <amaldev.psn@gmail.com>
2025-04-29 10:43:13 +0530

    c

--- namste_javascript/async_await/asynsIsSyntaticSugerPromise/asynsIsSyntaticSugerPromise.js
+++ namste_javascript/async_await/asynsIsSyntaticSugerPromise/asynsIsSyntaticSugerPromise.js
@@ -21,3 +21,16 @@ console.log("Script End");
 document.addEventListener('click', () => {
   console.log("User clicked the page!");
 });
+
+/* 
+Script Start
+asynsIsSyntaticSugerPromise.js:1
+Inside async function
+asynsIsSyntaticSugerPromise.js:4
+Script End
+asynsIsSyntaticSugerPromise.js:19
+Promise Resolved!
+asynsIsSyntaticSugerPromise.js:8
+Result after await: Done
+*/
+
