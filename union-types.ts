1cab9fbee376b51b7f4fbf8d072117d5d02c158c
Amaldev <amaldev.psn@gmail.com>
2025-06-28 21:34:42 +0530

    update namste_javascript/async_await/eventBubblingEventCapturing/index.html

---
+++ union-types.ts
@@ -0,0 +1,8 @@
+type frequency = 'monthly' | 'daily' | 'weekly';
+let myFrequency: frequency;
+myFrequency = 'daily';
+myFrequency = 'yearly';
+// --------------------------------------------
+let test: number | string;
+test = 123
+test = true;
